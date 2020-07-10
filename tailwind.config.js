ae39fa1a0f108562a7ce9b7a2b064899dc7a3c1a
rashadg1030 <rashad.sasaki@gmail.com>
2020-07-10 02:47:46 -0700

    Add hero; Add timeline; add other components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     extend: {
       colors: {
         blue: '#48b5e0',
-        green: '#71eeb8'
+        green: '#71eeb8',
+        pearl: '#EAE0C8',
+        coral: '#FF7F50',
       }
     },
   },
