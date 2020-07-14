829434969507e28e2df02493f9ab99aad202417a
rashadg1030 <rashad.sasaki@gmail.com>
2020-07-14 04:20:24 -0700

    Add random markdown generator

---
+++ html-gen/src/RandomMd.hs
@@ -0,0 +1,20 @@
+module RandomMd where
+
+{-
+This module is for generating random blog posts for tests.
+We are mainly interested in the top portion of the file.
+---
+id: 1
+title: Mini Pearl 1
+description: This is the first Mini Pearl.
+author: Rashad Gover
+date: 11/07/20
+tags:
+  - mini-pearl
+  - functional-programming
+  - theory
+---
+
+# Mini Pearl 1
+
+-}
